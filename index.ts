7a98ef7ddfada6d3984ee559f53e8c535266a53c
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-02-23 14:11:44 +0100

    smiplify email provider selection logic with early return

--- index.ts
+++ index.ts
@@ -5,68 +5,55 @@ import { sendEmail as sendEmailPostmark } from "./adapters/postmark";
 export function createClient(providers: Provider[]) {
 	return {
 		async sendEmail(email: SendEmailInput) {
-			let result;
 			for (const provider of providers) {
 				const providerType = provider.type;
 				try {
 					switch (providerType) {
-						case "custom":
-							{
-								const res = await provider.sendEmail(email);
-								if (res.error) {
-									throw new Error(res.error);
-								}
-								result = {
-									type: providerType,
-									index: providers.indexOf(provider),
-									custom: res,
-								};
+						case "custom": {
+							const res = await provider.sendEmail(email);
+							if (res.error) {
+								throw new Error(res.error);
 							}
-							break;
+							return {
+								type: providerType,
+								index: providers.indexOf(provider),
+								custom: res,
+							};
+						}
 
-						case "resend":
-							{
-								const res = await sendEmailResend(provider, email);
-								if (res.error) {
-									throw res.error;
-								}
-								result = {
-									type: providerType,
-									index: providers.indexOf(provider),
-									resend: res as any,
-								};
+						case "resend": {
+							const res = await sendEmailResend(provider, email);
+							if (res.error) {
+								throw res.error;
 							}
-							break;
+							return {
+								type: providerType,
+								index: providers.indexOf(provider),
+								resend: res as any,
+							};
+						}
 
-						case "postmark":
-							{
-								const res = await sendEmailPostmark(provider, email);
-								if (res.ErrorCode) {
-									throw new Error(res.Message);
-								}
-								result = {
-									type: providerType,
-									index: providers.indexOf(provider),
-									postmark: res,
-								};
+						case "postmark": {
+							const res = await sendEmailPostmark(provider, email);
+							if (res.ErrorCode) {
+								throw new Error(res.Message);
 							}
-							break;
+							return {
+								type: providerType,
+								index: providers.indexOf(provider),
+								postmark: res,
+							};
+						}
 
 						default:
 							providerType satisfies never;
 							throw new Error("Invalid provider type");
 					}
-					if (result) {
-						break;
-					}
 				} catch (error) {
 					console.error(error);
 				}
 			}
-			if (!result) {
-				throw new Error("No email provider available");
-			}
-			return result;
+			throw new Error("No email provider available");
 		},
 	};
 }
